176cd2a500772171b6a5e7b9868d6fc31f7fdf25
滕 博 <420912453@qq.com>
2022-03-28 11:03:26 +0800

    3

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -3,6 +3,26 @@ import '@/dom2'
 import '@/style/login.scss'
 export default defineComponent({
   setup () {
- 
+    class a {
+      static [Symbol.hasInstance] () {
+        return true
+      }
+      say () {
+        console.log('xxxx')
+      }
+    }
+    let obj = {
+      __proto__: a.prototype,
+      [Symbol.hasInstance] () {
+        return true
+      },
+      log () {
+        super.say()
+      }
+    }
+
+    // console.log(Array instanceof (obj as any))
+    console.dir(obj)
+    obj.log()
   }
 })
