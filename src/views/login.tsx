6107464e81847d779434cc416149db9c40528298
TBLZ <420912453@qq.com>
2022-04-07 22:07:54 +0800

    1

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -41,6 +41,7 @@ export default defineComponent({
         setTimeout(() => {      //++++++++
           self.callbacks.forEach(item => {
             item.onRejected(data)
+            
           })
         });
 
